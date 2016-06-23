db340c912e589cef454b76640904aeae1c3cba92
Dave Cheney <dave@cheney.net>
2016-06-23 22:19:43 +1000

    Added mroe dependencies
    Damnit if docker doesn't depend on every line of Go code ever written,
    most of it unreleased.

---
+++ vendor/src/github.com/google/certificate-transparency/java/src/org/certificatetransparency/ctlog/MerkleTreeLeaf.java
@@ -0,0 +1,17 @@
+package org.certificatetransparency.ctlog;
+
+import org.certificatetransparency.ctlog.TimestampedEntry;
+import org.certificatetransparency.ctlog.proto.Ct;
+
+
+public class MerkleTreeLeaf {
+  public Ct.Version version;
+  public Ct.MerkleLeafType type;
+  public TimestampedEntry timestampedEntry;
+
+  public MerkleTreeLeaf(Ct.Version version, Ct.MerkleLeafType type, TimestampedEntry timestamped_entry) {
+    this.version = version;
+    this.type = type;
+    this.timestampedEntry = timestamped_entry;
+  }
+}
